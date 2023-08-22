485cd24755da7a88d563cef48291992bc681efa7
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-08-22 16:12:53 +0200

    ddd

--- pages/index.vue
+++ pages/index.vue
@@ -40,28 +40,10 @@
         </button>
         
       </div>
-      <footer class="bg-neutral-900 rounded-lg m-4  ">
-        <div class="w-screen-xl items-center p-4 flex flex-col md:flex-row md:justify-between">
-            <span class="text-md text-white sm:text-center ">© 2023 
-                <a class="">Projet Collection Hugo, Tous droits réservés</a>
-        
-            </span>
-            <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white mr-0 gap-4 md:mr-16 sm:mt-0">
-                <li>
-                    <a href="https://www.linkedin.com/in/hugo-rytlewski-b06841281/">
-                    <img  class="w-6" src="~/assets/linkdinwhite.png" alt="">  
-                    </a>  
-                </li>
-                <li>
-                    <a href="https://github.com/HugoRytlewski">
-                      <img  class="w-6" src="~/assets/githubwhite.png" alt="">    
-                    </a>
-                </li>
-                
-            </ul>
-        </div>
-    </footer>
+      
     </div>
+   
+   
    
   </div>
 </template>
